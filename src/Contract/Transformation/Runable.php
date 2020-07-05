70ada1192b82a9c7c0e346300b897fd4fffe8a52
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-05 18:04:13 +0200

    Update Runable interface.

--- src/Contract/Transformation/Runable.php
+++ src/Contract/Transformation/Runable.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace loophp\collection\Contract\Transformation;
 
+use loophp\collection\Contract\Collection;
 use loophp\collection\Contract\Operation;
 
 interface Runable
 {
-    /**
-     * @param \loophp\collection\Contract\Operation ...$operations
-     *
-     * @return \loophp\collection\Base|\loophp\collection\Collection
-     */
-    public function run(Operation ...$operations);
+    public function run(Operation ...$operations): Collection;
 }
